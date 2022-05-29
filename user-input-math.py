4168b957f0c67d2ea9f9a4d787826efa67064d02
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-05-29 11:40:56 +0100

    Add files via upload

---
+++ user-input-math.py
@@ -0,0 +1,10 @@
+firstnumber = int(input("Please Enter The First Number: "))
+secondnumber = int(input("Please Enter The Second Number: "))
+addresult = firstnumber + secondnumber
+minusresult = firstnumber - secondnumber
+multiplyresult = firstnumber * secondnumber
+divideresult = int(firstnumber / secondnumber)
+print(addresult)
+print(minusresult)
+print(multiplyresult)
+print(divideresult)
